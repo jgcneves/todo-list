09806a99f944161b96cbb0a7b6024b26695a888f
João Neves <joao.neves@agenciaribalta.com>
2024-12-23 09:59:21 -0300

    Creates the TaskService

---
+++ app/Services/TaskService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\Repositories\TaskRepositoryInterface;
+use App\Contracts\Services\TaskServiceInterface;
+
+class TaskService implements TaskServiceInterface
+{
+    private TaskRepositoryInterface $taskRepository;
+
+    public function __construct(TaskRepositoryInterface $taskRepository)
+    {
+        $this->taskRepository = $taskRepository;
+    }
+
+    public function getAllTasks()
+    {
+        return $this->taskRepository->getAll();
+    }
+
+    public function createTask(array $data)
+    {
+        return $this->taskRepository->create($data);
+    }
+
+    public function getTaskById(int $id)
+    {
+        return $this->taskRepository->findById($id);
+    }
+
+    public function updateTask(int $id, array $data)
+    {
+        return $this->taskRepository->update($id, $data);
+    }
+
+    public function deleteTask(int $id)
+    {
+        $this->taskRepository->delete($id);
+    }
+}
